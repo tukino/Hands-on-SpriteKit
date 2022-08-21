e4b9245a0fa430306e692faf96bc3bd22c5ac3a0
tukino <siriuss_stars@hotmail.com>
2022-08-21 15:14:42 +0900

    Initial Commit

---
+++ HandsonSpriteKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HandsonSpriteKit
+//
+//  Created by tukino_salva on 2022/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HandsonSpriteKit/HandsonSpriteKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandsonSpriteKitApp.swift
+//  HandsonSpriteKit
+//
+//  Created by tukino_salva on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct HandsonSpriteKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
